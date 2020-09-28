1ba8c9c2e26a201125de3bd0400b8aee8bd27524
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 18:10:10 -0300

    Cadastrando Projetos

--- nivel-01/02-frontend-com-reactjs/src/App.js
+++ nivel-01/02-frontend-com-reactjs/src/App.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+import api from './services/api';
 
 import './App.css';
 
@@ -11,15 +13,30 @@ import Header from './components/Header';
  */
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
 
   // useState retorna um array com 2 posições
   //
   // 1. Variável com seu valor inicial
   // 2. Função para atualizarmos esse valor
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+  
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Danilo Vieira'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
@@ -27,7 +44,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}></button>
