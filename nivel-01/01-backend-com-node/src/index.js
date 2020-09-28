6ec4511d276c19c66806907904fccc6ec1da5f4d
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 16:04:36 -0300

    Aplicacao funcional

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -21,48 +22,60 @@ app.use(express.json());
   * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
   */
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  // const { title, owner } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  // console.log(title);
+  // console.log(owner);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
 
-  console.log(title);
-  console.log(owner);
+  const project ={ id: uuid(), title, owner };
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
+
+  const project = { 
+    id,
+    title, 
+    owner 
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
