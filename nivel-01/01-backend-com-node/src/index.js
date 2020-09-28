e3659a6c1d935bd8ff20a18675f1419f21c02214
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 16:19:00 -0300

    Middlewares

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -14,23 +14,55 @@ app.use(express.json());
  * DELETE: Deletar uma informação no back-end
  */
 
- /**
-  * Tipos de parâmetros:
-  * 
-  * Query Params: Filtros e paginação
-  * Route Params: Identificar recursos (Atualizar/Deletar)
-  * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
-  */
+/**
+* Tipos de parâmetros:
+* 
+* Query Params: Filtros e paginação
+* Route Params: Identificar recursos (Atualizar/Deletar)
+* Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+*/
+
+/**
+ * Middleware:
+ * 
+ * Interceptador de requisições que pode interromper totalmente a requisição ou alterar dados da requisição.
+ */
 
 const projects = [];
 
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUppercase()} ${url}]`;
+
+  console.time(logLabel);
+
+  next(); // Próximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
 app.get('/projects', (request, response) => {
-  // const { title, owner } = request.query;
+  const { title } = request.query;
 
-  // console.log(title);
-  // console.log(owner);
+  const results = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-  return response.json(projects);
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
