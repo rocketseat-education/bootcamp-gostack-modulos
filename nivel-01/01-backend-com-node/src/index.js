5a5f49dda2f4040d2a4471c72cc3711053b5cebb
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 15:05:18 -0300

    Configurando Nodemon

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -6,4 +6,6 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 Back-end started')
+});
