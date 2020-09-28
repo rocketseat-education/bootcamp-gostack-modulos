ad2692ff1c315c2e9d81ad6b3632a74990f07e6b
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 17:50:48 -0300

    Importando CSS e Imagens

--- nivel-01/02-frontend-com-reactjs/webpack.config.js
+++ nivel-01/02-frontend-com-reactjs/webpack.config.js
@@ -17,6 +17,20 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        }
       }
     ]
   }
