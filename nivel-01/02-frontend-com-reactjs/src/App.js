216b30e1e6fbf66bc243ef50685df03962b011e1
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 17:22:30 -0300

    Propriedades

--- nivel-01/02-frontend-com-reactjs/src/App.js
+++ nivel-01/02-frontend-com-reactjs/src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 import Header from './components/Header';
 
+/**
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
--- nivel-01/02-frontend-com-reactjs/src/components/Header.js
+++ nivel-01/02-frontend-com-reactjs/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({ title, children }) {
   return (
     <header>
-      <h1>ReactJS</h1>
+      <h1>{title}</h1>
+      
+      {children}
     </header>
   )
 }
