157cb5b8219c53b28f2108021675ed54f4535684
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-28 16:54:19 -0300

    Configurando babel

---
+++ nivel-01/02-frontend-com-reactjs/public/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var soma = function soma(a, b) {
+  return a + b;
+};
+
+console.log(soma(1, 3));
---
+++ nivel-01/02-frontend-com-reactjs/src/index.js
@@ -0,0 +1,5 @@
+const soma = (a, b) => {
+  return a + b;
+}
+
+console.log(soma(1, 3));
