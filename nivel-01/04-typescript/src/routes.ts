95c29460a1589a0d25a8e1e8e6d3e21159e547ed
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-29 14:52:25 -0300

    Tipando objetos e vetores

--- nivel-01/04-typescript/src/routes.ts
+++ nivel-01/04-typescript/src/routes.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import createUser from './services/createUser';
+
+// string, number, boolean, object, Array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'danilo@example.com',
+    password: '123456',
+    techs: [
+      'Node.js', 
+      'ReactJS', 
+      'React Native',
+      { title: 'JavaScript', experience: 100 }
+    ],
+  });
+
   return response.json({ message: 'Hello World' });
 }
