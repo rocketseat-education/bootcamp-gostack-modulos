1b1f12d6d2c342cc4d01492c6d4722a9f957d65d
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-29 16:23:37 -0300

    Model de Agendamento

--- nivel-02/01-primeiro-projeto-com-nodejs/src/routes/appointments.routes.ts
+++ nivel-02/01-primeiro-projeto-com-nodejs/src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -26,11 +20,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
