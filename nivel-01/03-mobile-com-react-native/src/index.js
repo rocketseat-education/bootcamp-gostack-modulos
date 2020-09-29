e1f1b7007d9c5fab1e1f1cc150a58c95bd4a988b
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-29 12:18:28 -0300

    Diferenças do ReactJS

--- nivel-01/03-mobile-com-react-native/src/index.js
+++ nivel-01/03-mobile-com-react-native/src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+// Não possuem valor semântico (significado)
+// Não possuem estilização própria
+// Todos componentes possuem por padrão "display: flex"
+
+// View: div, footer, header, main, aside, section
+// Text: p, span, strong, h1, h2, h3
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+});
