56f854a6614f386a27241021e2603a5632a899e1
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-29 16:07:47 -0300

    Cadastro de Agendamentos

---
+++ nivel-02/01-primeiro-projeto-com-nodejs/src/routes/appointments.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- nivel-02/01-primeiro-projeto-com-nodejs/src/routes/index.ts
+++ nivel-02/01-primeiro-projeto-com-nodejs/src/routes/index.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
