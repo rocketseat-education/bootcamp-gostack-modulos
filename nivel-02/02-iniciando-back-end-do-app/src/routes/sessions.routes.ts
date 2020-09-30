eb8f8a273870b05b046c7dd49819749d5788ec9a
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-09-30 15:50:10 -0300

    Validando credenciais

---
+++ nivel-02/02-iniciando-back-end-do-app/src/routes/sessions.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ user: userWithoutPassword });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
---
+++ nivel-02/02-iniciando-back-end-do-app/src/services/AuthenticateUserService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw Error('Incorrect email/password combination');
+    }
+
+    return {
+      user,
+    };
+  }
+}
+
+export default AuthenticateUserService;
