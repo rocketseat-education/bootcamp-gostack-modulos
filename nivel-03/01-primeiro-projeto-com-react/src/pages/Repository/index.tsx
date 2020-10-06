884765b06582045179e6cccf0c3fa671ca3e18ae
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-10-06 17:11:04 -0300

    Estilizando Detalhe

--- nivel-03/01-primeiro-projeto-com-react/src/pages/Repository/index.tsx
+++ nivel-03/01-primeiro-projeto-com-react/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +12,56 @@ interface RepositoryParams {
 
 const Dashboard: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <div>Repository:{params.repository}</div>;
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>asdasdasd</strong>
+            <p>asdasdasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Dashboard;
