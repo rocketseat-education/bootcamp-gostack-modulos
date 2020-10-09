3a4dd9321d9836085c024ddaf1442e4888da2511
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-10-09 13:38:20 -0300

    Estilos globais

--- nivel-03/02-iniciando-o-front-end-web/src/App.tsx
+++ nivel-03/02-iniciando-o-front-end-web/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/globals';
+
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
