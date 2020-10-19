9b89ee0f960ab922e338eeda7e70298bb9f90c70
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-10-19 13:10:06 -0300

    Página de cadastro

--- nivel-03/02-iniciando-o-front-end-web/src/App.tsx
+++ nivel-03/02-iniciando-o-front-end-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
   );
---
+++ nivel-03/02-iniciando-o-front-end-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="E-mail" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="signup">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
