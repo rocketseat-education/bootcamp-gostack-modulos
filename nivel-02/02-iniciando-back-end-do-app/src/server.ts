d9c4608244c047c434f8008976fcfb93eb2cb800
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-10-19 17:58:01 -0300

    Habilitando CORS na API

--- nivel-02/02-iniciando-back-end-do-app/src/server.ts
+++ nivel-02/02-iniciando-back-end-do-app/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
