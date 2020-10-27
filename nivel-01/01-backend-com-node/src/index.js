781ff41fbc3240c4693a82c88c9e05995c9f3df3
Tiago Conceição <52356356+tiagorconceicao@users.noreply.github.com>
2020-10-27 13:14:04 -0300

    add cors use
    cors added on lesson:
    
    https://app.rocketseat.com.br/node/front-end-com-react-js/lesson/listando-projetos-da-api-1

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 /**
