1f12ad5a6182c9c29f710a1d8f47dad0dfd69df8
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-11-12 17:02:06 -0300

    Importando fontes externas

--- nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
+++ nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,6 +9,8 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
