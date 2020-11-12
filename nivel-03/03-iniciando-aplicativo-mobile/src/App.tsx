56a2ef80e40555619d6d620889cf22209a1fc30d
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-11-12 16:30:22 -0300

    Configurando navegação

--- nivel-03/03-iniciando-aplicativo-mobile/src/App.tsx
+++ nivel-03/03-iniciando-aplicativo-mobile/src/App.tsx
@@ -1,11 +1,18 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
---
+++ nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SignIn;
