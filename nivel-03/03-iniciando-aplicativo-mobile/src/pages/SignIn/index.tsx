d698deaf45a620f72a9ecaf6826cbb4699322e4c
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-11-13 15:25:19 -0300

    Tela de cadastro

--- nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
+++ nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
@@ -7,6 +7,7 @@ import {
   Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -23,6 +24,8 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -54,7 +57,7 @@ const SignIn: React.FC = () => {
         </ScrollView>
       </KeyboardAvoidingView>
 
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
 
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
--- nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/styles.ts
+++ nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
 export const Title = styled.Text`
