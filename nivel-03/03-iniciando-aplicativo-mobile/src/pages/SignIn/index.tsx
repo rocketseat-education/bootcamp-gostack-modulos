ffff123602cdba5bb052d4552208d70d9379424c
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-11-13 14:32:59 -0300

    Input & Button

--- nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
+++ nivel-03/03-iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -11,6 +14,12 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => console.log('Deu')}>Entrar</Button>
     </Container>
   );
 };
