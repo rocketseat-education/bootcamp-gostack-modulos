ae1cad21f87dd9da65355546ad1d7e1f8f8e700d
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-12-08 12:31:11 -0300

    Aplicando TDD na prática

---
+++ nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+
+// import User from '../infra/typeorm/entities/User';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    await this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
