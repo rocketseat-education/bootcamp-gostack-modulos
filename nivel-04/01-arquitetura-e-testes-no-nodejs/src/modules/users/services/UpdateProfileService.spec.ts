d453b8b192d8f67c41c8467643dd7ce284942179
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-12-17 18:08:30 -0300

    Rota e controller de perfil

--- nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/users/services/UpdateProfileService.spec.ts
+++ nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntre@example.com');
   });
 
+  it('should not be able to update the profile from a non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
