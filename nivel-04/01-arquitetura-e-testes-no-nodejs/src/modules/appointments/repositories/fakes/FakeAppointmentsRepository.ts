fa870f65d7d6d5aff2423aafb209a94e3cdd8fe0
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-04 15:27:10 -0300

    Filtrando agendamentos por mês

--- nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,8 +1,9 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import IAppointmentsRepository from '../IAppointmentsRepository';
 
 export default class FakeAppointmentsRepository
@@ -15,6 +16,21 @@ export default class FakeAppointmentsRepository
     );
 
     return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+
+    return appointments;
   }
 
   public async create({
---
+++ nivel-04/01-arquitetura-e-testes-no-nodejs/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    console.log(appointments);
+
+    return [{ day: 1, available: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
