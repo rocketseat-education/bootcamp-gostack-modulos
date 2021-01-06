ef3c8a3bbb56d39bf4d6d1d80ab2ca165ea367ec
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-06 16:33:38 -0300

    Emails pelo Amazon SES

---
+++ nivel-04/02-finalizando-backend-do-app/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'seu@email.com',
+      name: 'Seu Nome',
+    },
+  },
+} as IMailConfig;
---
+++ nivel-04/02-finalizando-backend-do-app/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,48 @@
+import { injectable, inject } from 'tsyringe';
+import nodemailer, { Transporter } from 'nodemailer';
+import aws from 'aws-sdk';
+
+import mailConfig from '@config/mail';
+
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailProvider from '../models/IMailProvider';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+
+@injectable()
+export default class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'us-east-1',
+      }),
+    });
+  }
+
+  public async sendMail({
+    from,
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
+  }
+}
