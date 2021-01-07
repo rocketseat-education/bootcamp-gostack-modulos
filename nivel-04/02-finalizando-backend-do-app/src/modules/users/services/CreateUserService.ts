bc380e1dd509557ecd6c2b8343c27ea7be68eedb
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-07 15:01:05 -0300

    Invalidando cache

--- nivel-04/02-finalizando-backend-do-app/src/modules/users/services/CreateUserService.ts
+++ nivel-04/02-finalizando-backend-do-app/src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
@@ -20,6 +21,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -36,6 +40,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
