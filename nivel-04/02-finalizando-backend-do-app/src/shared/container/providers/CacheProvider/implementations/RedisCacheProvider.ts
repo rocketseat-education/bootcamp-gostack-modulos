33453815c9597a7ce56b29a1541ec8a9500eb31a
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-07 14:20:15 -0300

    Cache lista de providers

--- nivel-04/02-finalizando-backend-do-app/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ nivel-04/02-finalizando-backend-do-app/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -11,14 +11,20 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
-  public async save(key: string, value: string): Promise<void> {
+  public async save(key: string, value: any): Promise<void> {
     await this.client.set(key, JSON.stringify(value));
   }
 
-  public async recover(key: string): Promise<string | null> {
+  public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key);
 
-    return data;
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
   }
 
   public async invalidate(key: string): Promise<void> {}
--- nivel-04/02-finalizando-backend-do-app/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ nivel-04/02-finalizando-backend-do-app/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -1,5 +1,5 @@
 export default interface ICacheProvider {
-  save(key: string, value: string): Promise<void>;
-  recover(key: string): Promise<string | null>;
+  save(key: string, value: any): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
   invalidate(key: string): Promise<void>;
 }
