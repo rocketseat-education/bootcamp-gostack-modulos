74f6f429d82e8de4aca68c494474c7c65c109cac
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-11 11:11:28 -0300

    Clientes dos agendamentos

--- nivel-04/02-finalizando-backend-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ nivel-04/02-finalizando-backend-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -59,6 +59,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, DD-MM-YYYY) = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
