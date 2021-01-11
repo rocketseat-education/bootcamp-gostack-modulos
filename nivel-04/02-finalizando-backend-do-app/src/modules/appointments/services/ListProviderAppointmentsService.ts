fcf38dd407bf4f82144b32725c5b78a9877ca693
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-11 11:45:51 -0300

    Serialização no cache

--- nivel-04/02-finalizando-backend-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ nivel-04/02-finalizando-backend-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
       );
     }
 
-    await this.cacheProvider.save(cacheKey, appointments);
+    await this.cacheProvider.save(cacheKey, classToClass(appointments));
 
     return appointments;
   }
