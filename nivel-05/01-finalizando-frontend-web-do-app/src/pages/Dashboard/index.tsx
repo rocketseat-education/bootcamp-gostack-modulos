898833a2c05033869ae08fa0ae47d5cc68c0aa10
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-11 17:01:39 -0300

    Próximo agendamento

--- nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
+++ nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,6 +38,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendadoos</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="/avatar_url" alt="Nome do usuário" />
+
+              <strong>Nome do usuário</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   );
 };
