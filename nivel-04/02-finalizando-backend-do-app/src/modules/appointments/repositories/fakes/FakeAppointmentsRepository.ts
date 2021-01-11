89f04cf4f4126d8cb0d4239d8f51c35fd3bbe355
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-11 10:59:24 -0300

    Agendamentos no mesmo horário

--- nivel-04/02-finalizando-backend-do-app/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ nivel-04/02-finalizando-backend-do-app/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -11,9 +11,10 @@ export default class FakeAppointmentsRepository
   implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
+  public async findByDate(date: Date, provider_id: string): Promise<Appointment | undefined> {
     const findAppointment = this.appointments.find(appointment =>
-      isEqual(appointment.date, date),
+      isEqual(appointment.date, date) &&
+      appointment.provider_id === provider_id,
     );
 
     return findAppointment;
--- nivel-04/02-finalizando-backend-do-app/src/modules/appointments/services/CreateAppointmentService.ts
+++ nivel-04/02-finalizando-backend-do-app/src/modules/appointments/services/CreateAppointmentService.ts
@@ -51,6 +51,7 @@ class CreateAppointmentService {
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
