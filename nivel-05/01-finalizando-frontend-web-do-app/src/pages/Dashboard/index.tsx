efdbfaa435bf430f7cb529d0ed3409afaf3cc10a
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-01-11 16:27:38 -0300

    Criando Header

--- nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
+++ nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
@@ -1,4 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 export default Dashboard;
